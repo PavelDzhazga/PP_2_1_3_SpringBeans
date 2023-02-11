923a6901fe7c49677e90edbea7fc45133146a5ac
Pavel Dzhazga <dzhazga2002@mail.ru>
2023-02-11 17:30:00 +0300

    plj

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,19 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog extends  Animal{
+    private String name;
+
+    public Dog() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
